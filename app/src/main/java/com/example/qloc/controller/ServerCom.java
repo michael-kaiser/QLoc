7fa3aa986b45d80440645764e220ed433e9ed9ce
uli <csad2028@uibk.ac.at>
2015-04-18 12:33:32 +0200

    JSON erweitert

---
+++ app/src/main/java/com/example/qloc/controller/ServerCom.java
@@ -0,0 +1,66 @@
+package com.example.qloc.controller;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Created by uli on 18.04.15.
+ */
+public class ServerCom {
+
+    public static String rangeQuery(double lon, double lat) throws IOException {
+
+        JsonFactory jFactory = new JsonFactory();
+        StringWriter writer = new StringWriter();
+        JsonGenerator jsonGenerator = null;
+        try {
+            jsonGenerator = jFactory.createGenerator(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("longitude", lon);
+        jsonGenerator.writeNumberField("latitude", lat);
+        jsonGenerator.writeEndObject();
+        jsonGenerator.close();
+        return writer.toString();
+    }
+
+    public static String sendAnswer(String ans) throws IOException {
+        JsonFactory jFactory = new JsonFactory();
+        StringWriter writer = new StringWriter();
+        JsonGenerator jsonGenerator = null;
+        try {
+            jsonGenerator = jFactory.createGenerator(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("answer", ans);
+        jsonGenerator.writeEndObject();
+        jsonGenerator.close();
+        return writer.toString();
+    }
+
+    public static boolean evaluatedAnswer(String st) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj = null;
+        try {
+            actualObj = mapper.readTree(st);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JsonNode aField = actualObj.get("evaluation");
+
+
+        return "true".equals(aField.toString());
+
+    }
+
+
+}
